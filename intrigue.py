6a77bb6a9308ff7bfb11c00a595891f9eda0eb0a
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-19 23:44:56 +0300

    Completed assignment

---
+++ intrigue.py
@@ -0,0 +1,46 @@
+# name = "JOHN" 
+# print(name)
+# #creating user inputs in python
+# namer = input("What is your name? ")
+# print(namer)
+geans = "johney"
+print(geans)
+print(f"i want to see {geans}")
+#functions in python
+def player(number1,number2):
+  print(number1 * number2)
+player(23,66)
+# def average(num1,num2):
+#   addition = num1 + num2
+#   return addition / 2
+
+# #   print(addition / 2)
+#   print(average(100,20))
+#while loop
+# loop = 10
+# while loop <100:
+#   print(loop)
+#   print("Hello there!")
+#   loop = loop + 1
+#   #for loop
+# i = 0 
+# for i in range(10):
+#   print(i)
+programming_languages = ["Python","Java","JavaScript","Ruby"]
+# for i in range(len(programming_languages)):
+#    print(programming_languages[i])
+# for  k in programming_languages:
+#   print(k)
+# thisdict = {
+#   "brand": "Ford",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# }
+# for key,value in thisdict.items():
+#   print("name of book:" + key)
+#   print(value)
+#reverse looping
+persons  = [1, 2, 'Python', 'Program', 15.9]
+for people in reversed(persons):
+  print(people)
